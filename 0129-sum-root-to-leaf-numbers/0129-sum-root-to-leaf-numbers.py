582efd1bcc9f930ecd8b37a41cfc8b8dbac6c76c
Divya Eeli <119840639+22A91A61E8@users.noreply.github.com>
2024-06-10 23:39:45 +0530

    Time: 34 ms (68.86%), Space: 16.4 MB (74.20%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
@@ -5,16 +5,17 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def divya(self, root, s, total_sum):
+    def divya(self,root,s,total_sum):
         if root is None:
             return
         if root.left is None and root.right is None:
-            total_sum[0] += (s * 10 + root.val)
-            return
-        self.divya(root.left, s * 10 + root.val, total_sum)
-        self.divya(root.right, s * 10 + root.val, total_sum)
-        
+            total_sum[0]+=(s*10+root.val)
+        self.divya(root.left,s*10+root.val,total_sum)
+        self.divya(root.right,s*10+root.val,total_sum)
+            
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        total_sum = [0]
-        self.divya(root, 0, total_sum)
+        s=0
+        total_sum=[0]
+        self.divya(root,0,total_sum)
         return total_sum[0]
+        
