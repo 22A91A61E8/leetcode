016da2fab10decf105688f29c6290049ac8202e9
Divya Eeli <119840639+22A91A61E8@users.noreply.github.com>
2024-06-10 19:43:32 +0530

    Time: 34 ms (85.79%), Space: 16.4 MB (97.08%) - LeetHub

--- 1051-height-checker/1051-height-checker.py
+++ 1051-height-checker/1051-height-checker.py
@@ -1,5 +1,6 @@
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
+        #to create a newlist form existing should use always sorted() where as sort() will modify the existing one and return none
         e=sorted(heights)
         c=0
         for i in range(len(heights)):
