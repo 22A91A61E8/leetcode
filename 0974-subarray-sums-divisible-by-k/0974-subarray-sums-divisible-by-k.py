d559f69868aab5b7fed750919a79f2f11197541c
Divya Eeli <119840639+22A91A61E8@users.noreply.github.com>
2024-06-16 22:13:51 +0530

    Time: 221 ms (86.64%), Space: 21.4 MB (33.05%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
@@ -1,25 +1,16 @@
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
-        prefix_sum = 0
-        count = 0
-        mod_count = {0: 1}  # Initialize with 0:1 to handle the case where the prefix sum itself is divisible by k
-        
-        for num in nums:
-            prefix_sum += num
-            mod = prefix_sum % k
-            
-            # If mod is negative, convert it to a positive equivalent
-            if mod < 0:
-                mod += k
-            
-            # If mod is in the hashmap, it means we have seen this mod value before
-            if mod in mod_count:
-                count += mod_count[mod]
-            
-            # Update the count of this mod value in the hashmap
-            if mod in mod_count:
-                mod_count[mod] += 1
+        s=0
+        d={}
+        c=0
+        for i in nums:
+            s=s+i
+            r=s%k
+            if r not in d:
+                d[r]=1
             else:
-                mod_count[mod] = 1
-        
-        return count
+                c=c+d[r]
+                d[r]+=1
+            if(r==0):
+                c=c+1
+        return c
