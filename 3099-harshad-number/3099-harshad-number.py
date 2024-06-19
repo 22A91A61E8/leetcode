9abe2d87db4d81e316d8e9837e420ef3f0aa256c
Divya Eeli <119840639+22A91A61E8@users.noreply.github.com>
2024-06-19 23:50:00 +0530

    Time: 22 ms (99.21%), Space: 16.5 MB (84.12%) - LeetHub

---
+++ 3099-harshad-number/3099-harshad-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sumOfTheDigitsOfHarshadNumber(self, x: int) -> int:
+        s=0
+        for i in str(x):
+            s=s+int(i)
+       
+        if(x%s==0):
+            return s
+        else:
+            return -1
