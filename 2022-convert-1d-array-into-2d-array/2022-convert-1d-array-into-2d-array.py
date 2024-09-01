8b84e9e86403f02a8105b1b57e308944a6ec4330
Divya Eeli <119840639+22A91A61E8@users.noreply.github.com>
2024-09-01 16:12:39 +0530

    Time: 942 ms (5.18%), Space: 34.4 MB (6.97%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.py
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.py
@@ -1,7 +1,7 @@
+import numpy as np
 class Solution:
     def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
-        r=[]
-        if m*n==len(original):
-            for i in range(0,m*n,n):
-                r.append(original[i:i+n])
+        if len(original)!=m*n:
+            return []
+        r=np.array(original).reshape(m,n)
         return r
